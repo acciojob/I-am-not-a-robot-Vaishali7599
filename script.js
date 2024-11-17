905e502945b8390e9b42d9dcad9b7bef5fa2e8e3
Vaishali7599 <140495282+Vaishali7599@users.noreply.github.com>
2024-11-17 23:12:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your code here
+let container = document.querySelector("main");
+let imagesClassNames = ["img1", "img2", "img3", "img4", "img5"];
+
+let randomIndex = parseInt(Math.random()*imagesClassNames.length);
+
+imagesClassNames.push(imagesClassNames[randomIndex])
+console.log(imagesClassNames)
+
+
+for(let t of imagesClassNames){
+	let pic = document.createElement("img");
+	pic.className = t;
+	pic.alt = t;
+	container.append(pic);
+	
+	
+	
+}
+
